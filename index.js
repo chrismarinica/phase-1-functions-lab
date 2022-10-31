2981da707c1087b77c6e8ea1f3cbe34e339a3335
Christopher Marinica <chris.marinica@gmail.com>
2022-10-31 18:20:02 -0400

    commit tree 64dbf75bad7e0cb1b6272e02cc45db8d238bc082

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block){
+    const hq = 42;
+    return Math.abs(hq-block);
+}
+
+function distanceFromHqInFeet(blocks) {
+    return distanceFromHqInBlocks(blocks) * 264;
+  }
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) *264;
+}
+
+function calculatesFarePrice(start, destination) {
+    if((Math.abs(destination - start)*264) <= 400){
+        return 0;
+    }
+    else if ((Math.abs(destination - start)*264) <= 2000){
+        return 2.56;
+    }
+    else if ((Math.abs(destination - start)*264) <= 2500){
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+    
+}
